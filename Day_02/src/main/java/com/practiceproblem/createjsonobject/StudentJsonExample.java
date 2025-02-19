1b308e75e015cd489cc96049f0061f84beb794d1
unknown <soniriya2409@gmail.com>
2025-02-19 16:34:20 +0530

    Create a JSON object for a Student

---
+++ Day_02/src/main/java/com/practiceproblem/createjsonobject/StudentJsonExample.java
@@ -0,0 +1,21 @@
+package com.practiceproblem.createjsonobject;
+
+import org.json.JSONObject;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentJsonExample {
+    public static void main(String[] args) {
+        // Create a JSON object for Student
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("name", "John Doe");
+        studentJson.put("age", 20);
+
+        // Adding an array of subjects
+        List<String> subjects = Arrays.asList("Mathematics", "Computer Science", "Physics");
+        studentJson.put("subjects", subjects);
+
+        // Print the JSON representation
+        System.out.println(studentJson.toString(4));
+    }
+}
